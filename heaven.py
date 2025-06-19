3fc0c40078f2f49b4cc403b610e5a0cd19a30ecc
zhemingw <zhe.ming.wong@intel.com>
2025-06-19 14:22:27 +0800

    fixed and added heaven.py and vlc.py for yocto target

--- heaven.py
+++ heaven.py
@@ -96,11 +96,20 @@ def isRunningHeaven():
 # Main function with all the magic, Start -> Monitor -> Close after runtime
 def main(run_time):
     #dir_heaven = r'/data/applications/heaven/Unigine_Heaven-4.0/bin/heavenx64'
-    cmd_heaven = 'DISPLAY=:0 /data/applications/heaven/Unigine_Heaven-4.0-Pro/bin/heaven_x64 \
+#    cmd_heaven = 'DISPLAY=:0 /data/applications/heaven/Unigine_Heaven-4.0/bin/heaven_x64 \
+#        -video_app opengl -sound_app openal -project_name Heaven -data_path ../ \
+#            -system_script heaven/unigine.cpp -engine_config ../data/heaven_4.0.cfg -video_multisample 0 \
+#                -video_fullscreen 1 -video_mode -1 -video_width 1280 -video_height 720 -frame -1 -duration 0 -type 0 -level 0  \
+#                    -extern_plugin GPUMonitor -extern_define RELEASE,HEAVEN_ADV,AUTOMATION,QUALITY_ULTRA,TESSELLATION_NORMAL'
+                    
+    cmd_heaven = 'export LD_LIBRARY_PATH=/data/applications/heaven/Unigine_Heaven-4.0/bin:$LD_LIBRARY_PATH && \
+        DISPLAY=:0 /data/applications/heaven/Unigine_Heaven-4.0/bin/heaven_x64 \
         -video_app opengl -sound_app openal -project_name Heaven -data_path ../ \
-            -system_script heaven/unigine.cpp -engine_config ../data/heaven_4.0.cfg -video_multisample 0 \
-                -video_fullscreen 1 -video_mode -1 -video_width 1280 -video_height 720 -frame -1 -duration 0 -type 0 -level 0  \
-                    -extern_plugin GPUMonitor -extern_define RELEASE,HEAVEN_ADV,AUTOMATION,QUALITY_ULTRA,TESSELLATION_NORMAL'
+        -system_script heaven/unigine.cpp -engine_config ../data/heaven_4.0.cfg -video_multisample 0 \
+        -video_fullscreen 1 -video_mode -1 -video_width 1280 -video_height 720 -frame -1 -duration 0 -type 0 -level 0 \
+        -extern_plugin GPUMonitor -extern_define RELEASE,HEAVEN_ADV,AUTOMATION,QUALITY_ULTRA,TESSELLATION_NORMAL'
+    
+    
     start_time = time.time()
     logger.info('************** Executing Heaven iter = 1 **************')
     i=1
--- vlc.py
+++ vlc.py
@@ -1,41 +1,42 @@
+#!/usr/bin/python3
+# filepath: /root/validation/vlc.py
+
 __author__ = 'Lau, Shun Yang'
 __email__ = 'shun.yang.lau@intel.com'
 __version__ = '1.0.0'
 
 import os
 import sys
-import getopt
 import argparse
-import re
 import datetime
 import time
 import svtools.logging.toolbox as slt
 import subprocess
 from subprocess import Popen
 
-#Input Argument
+# Input Argument
 ap = argparse.ArgumentParser()
 group = ap.add_argument_group('required flags')
 group.add_argument('-t', '--time', dest='time', required=True, help='How long to run video playback')
-args= ap.parse_args()
+args = ap.parse_args()
 
-#Script & file path
-scriptpath = (os.path.abspath(__file__))
+# Script & file path - updated for Linux paths
+scriptpath = os.path.abspath(__file__)
 path = os.path.dirname(scriptpath)
-program_path = r"C:\Program Files (x86)\VideoLAN\VLC"
+program_path = "/home/vlcuser/VLC_mediaplayer"  # Path to VLC AppImage
 
-#Input for VLC video playback
+# Input for VLC video playback
 runtime = args.time
 
-#Log Path
-log_path = path+'\\Log\\VLC'
+# Log Path - updated for Linux paths
+log_path = os.path.join(path, 'Log', 'VLC')
 if not os.path.exists(log_path):
     os.makedirs(log_path)
-pylog_path = log_path+'\\vlc_py.log'
+pylog_path = os.path.join(log_path, 'vlc_py.log')
 
-#Logging setup
+# Logging setup
 _log = slt.getLogger('myscript', autosplit=True)
-_log.setFile(pylog_path, overwrite = True)
+_log.setFile(pylog_path, overwrite=True)
 _log.colorLevels = True
 _log.setConsoleFormat = 'simple'
 _log.setFileFormat = 'time'
@@ -43,47 +44,58 @@ _log.setConsoleLevel('INFO')
 _log.setFileLevel('DEBUGALL')
 
 def vid_cmd():
-    vid_k = "vlc.exe --extraintf=http:logger --verbose=3 -R C:\validation\windows-test-content\concurrency\content\execution\Playlist\Karma.mp4 --file-logging --logfile="+log_path+runlog_path_k
-    vid_m = "vlc.exe --extraintf=http:logger --verbose=3 -R C:\\Users\\Administrator\\Music\\Media\\Moment.mp4 --file-logging --logfile="+log_path+runlog_path_m
-    vid_n = "vlc.exe --extraintf=http:logger --verbose=3 -R C:\\Users\\Administrator\\Music\\Media\\NewZealand.mp4 --file-logging --logfile="+log_path+runlog_path_n
+    # Update video paths to Linux paths - adjust these to your actual video locations
+    video_path = "/root/validation/videos"  # Create this directory and place videos here
+    
+    # VLC commands updated for AppRun
+    vid_k = f"./AppRun --extraintf=http:logger --verbose=3 -R {video_path}/Karma.mp4 --file-logging --logfile={os.path.join(log_path, runlog_path_k)}"
+    vid_m = f"./AppRun --extraintf=http:logger --verbose=3 -R {video_path}/Moment.mp4 --file-logging --logfile={os.path.join(log_path, runlog_path_m)}"
+    vid_n = f"./AppRun --extraintf=http:logger --verbose=3 -R {video_path}/NewZealand.mp4 --file-logging --logfile={os.path.join(log_path, runlog_path_n)}"
 
-    _log.debug('command video karma: '+vid_k)
-    _log.debug('command video moment: '+vid_m)
-    _log.debug('command video new zealand: '+vid_n)
+    _log.debug('command video karma: ' + vid_k)
+    _log.debug('command video moment: ' + vid_m)
+    _log.debug('command video new zealand: ' + vid_n)
     _log.debug('Wait 5 seconds for other application to launch')
     time.sleep(5)
     
-    with open(log_path + runlog_path, 'w+') as LogFileHandler:
-        Popen(vid_k, cwd=program_path, shell=True, stdin=None, stderr=None, stdout=LogFileHandler)
-        Popen(vid_m, cwd=program_path, shell=True, stdin=None, stderr=None, stdout=LogFileHandler)
-        Popen(vid_n, cwd=program_path, shell=True, stdin=None, stderr=None, stdout=LogFileHandler)
+    # Create an environment variable with DISPLAY set
+    env = os.environ.copy()
+    env["DISPLAY"] = ":0"
+    env["VLC_IGNORE_UID_CHECK"] = "1"  # Add this if needed to run as root
+    
+    with open(os.path.join(log_path, runlog_path), 'w+') as LogFileHandler:
+        Popen(vid_k, cwd=program_path, shell=True, stdin=None, stderr=None, stdout=LogFileHandler, env=env)
+        Popen(vid_m, cwd=program_path, shell=True, stdin=None, stderr=None, stdout=LogFileHandler, env=env)
+        Popen(vid_n, cwd=program_path, shell=True, stdin=None, stderr=None, stdout=LogFileHandler, env=env)
     _log.info('Launched VLC with subprocess on separate thread')
 
 def vid_timer():
-    while(((time.time() - startTime)/60.0) <= int(runtime)):
-        print('Target run time = '+runtime)
+    while ((time.time() - startTime)/60.0) <= int(runtime):
+        print('Target run time = ' + runtime)
         print('Current run time = ', (time.time() - startTime)/60.0, " minutes")
         time.sleep(1)
-    subprocess.run(["taskkill", "/f", "/im", "vlc.exe"], shell=True)
-    _log.info("vlc.exe is killed")
+    
+    # Updated process killing for Linux
+    subprocess.run(["pkill", "-f", "AppRun"], shell=False)
+    _log.info("VLC processes killed")
 
 if __name__ == '__main__':
     dt = datetime.date.today()
     raw_time = datetime.datetime.now()
     tm = raw_time.strftime("%H-%M-%S")
 
-    runlog_path_k = '\\vlc_run_karma_'+str(dt)+'_'+str(tm)+'.log'
-    runlog_path_m = '\\vlc_run_moment_'+str(dt)+'_'+str(tm)+'.log'
-    runlog_path_n = '\\vlc_run_new_zealand_'+str(dt)+'_'+str(tm)+'.log'
-    runlog_path = '\\vlcout.log'
-
+    # Updated for Linux paths
+    runlog_path_k = f'vlc_run_karma_{str(dt)}_{str(tm)}.log'
+    runlog_path_m = f'vlc_run_moment_{str(dt)}_{str(tm)}.log'
+    runlog_path_n = f'vlc_run_new_zealand_{str(dt)}_{str(tm)}.log'
+    runlog_path = 'vlcout.log'
 
     startTime = time.time()
     
     try:
         vid_cmd()
-    except:
-        _log.error('VLC not launched')
+    except Exception as e:
+        _log.error(f'VLC not launched: {str(e)}')
         sys.exit(1)
     finally:
         vid_timer()
